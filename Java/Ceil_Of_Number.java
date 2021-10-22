b3abc0de440d5d1125b12a72e03926e2c09fdb78
Vaishnavi Bholane <87429198+vaishnavi-bholane@users.noreply.github.com>
2021-10-22 17:24:26 +0530

    Add files via upload
    Added calculation of ceil of a number using binary search

---
+++ Java/Ceil_Of_Number.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class Ceil_Of_Number {
+    public static void main(String[] args) {
+
+        int[] arr = {2,3,5,9,14,16,18};
+        int target = 15;
+        int ans = ceiling(arr,target);
+        System.out.println("Ceiling of number is: " + arr[ans]);
+    }
+    static int ceiling(int[] arr, int target)
+    {
+        if(target > arr[arr.length-1])
+        {
+            return -1;
+        }
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            if(target < arr[mid])
+            {
+                end = mid - 1;
+            }
+            else if(target > arr[mid])
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                return mid;
+            }
+        }
+        return start;
+    }
+}
