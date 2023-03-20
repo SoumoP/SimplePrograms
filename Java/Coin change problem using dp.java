ff627db23664839cf328dccb0d7e6ac4d3247dc8
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-03-20 22:53:00 +0530

    Update Coin change problem using dp.java

--- Java/Coin change problem using dp.java
+++ Java/Coin change problem using dp.java
@@ -9,27 +9,27 @@ class CoinChanger{
 
     int coinChangeProblem(int[] coins, int sum) {
 	    
-		int totalCoins = coins.length;
+	int totalCoins = coins.length;
 
-		// Creating array which stores subproblems' solutions
-		double[][] arr = new double[totalCoins + 1][sum + 1];
+	// Creating array which stores subproblems' solutions
+	double[][] arr = new double[totalCoins + 1][sum + 1];
 
-		// Initialising first row with +ve infinity
-		for (int j = 0; j <= sum; j++) {
-			arr[0][j] = Double.POSITIVE_INFINITY;
-		}	      
+	// Initialising first row with +ve infinity
+	for (int j = 0; j <= sum; j++) {
+		arr[0][j] = Double.POSITIVE_INFINITY;
+	}	      
 	    
-		// Initialising first column with 0
-		for (int i = 1; i <= totalCoins; i++) {
-			arr[i][0] = 0;
-		}
+	// Initialising first column with 0
+	for (int i = 1; i <= totalCoins; i++) {
+		arr[i][0] = 0;
+	}
 
-		// Implementing the recursive solution
-		for (int i = 1; i <= totalCoins; i++) {
-			for (int j = 1; j <= sum; j++) {
-				if (coins[i - 1] <= j) {
-                    arr[i][j] = min(1 + arr[i][j - coins[i - 1]], arr[i - 1][j]);
-				}
+	// Implementing the recursive solution
+	for (int i = 1; i <= totalCoins; i++) {
+		for (int j = 1; j <= sum; j++) {
+			if (coins[i - 1] <= j) {
+                arr[i][j] = min(1 + arr[i][j - coins[i - 1]], arr[i - 1][j]);
+			}
 				else {
 					arr[i][j] = arr[i - 1][j];
 			    }
